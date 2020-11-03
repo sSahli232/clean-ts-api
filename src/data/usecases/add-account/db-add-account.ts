1f7bade016898afcf44a20412659e504cce39e9f
sSahli232 <sahli.selim@gmail.com>
2020-11-03 21:41:09 +0100

    refactor: remove duplicated code

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -6,16 +6,10 @@ import {
   AddAccountRepository
 } from './db-add-account-protocols'
 export class DbAddAccount implements AddAccount {
-  private readonly hasher: Hasher
-  private readonly addAccountRepository: AddAccountRepository
-
   constructor (
-    hasher: Hasher,
-    addAccountRepository: AddAccountRepository
-  ) {
-    this.hasher = hasher
-    this.addAccountRepository = addAccountRepository
-  }
+    private readonly hasher: Hasher,
+    private readonly addAccountRepository: AddAccountRepository
+  ) {}
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.hasher.hash(accountData.password)
