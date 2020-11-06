59a59979c15973f446e109df44b4bbebc7926749
sSahli232 <sahli.selim@gmail.com>
2020-11-07 00:32:36 +0100

    feat: add AddSurvey composition

--- src/main/decorators/log-controller-decorator.ts
+++ src/main/decorators/log-controller-decorator.ts
@@ -9,7 +9,7 @@ export class LogControllerDecorator implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
-    if (httpResponse.statusCode) {
+    if (httpResponse.statusCode === 500) {
       await this.logErrorRepository.logError(httpResponse.body.stack)
     }
     return httpResponse
