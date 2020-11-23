88b97d9622c7224254f1dcd2f834ce901c9dec31
sSahli232 <sahli.selim@gmail.com>
2020-11-23 11:34:06 +0100

    docs: add load surveys api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,6 +1,6 @@
-import { loginPath } from './paths'
-import { badRequest, unauthorized, serverError, notFound } from './components'
-import { accountSchema, loginParamsSchema, errorSchema } from './schemas'
+import { loginPath, surveyPath } from './paths'
+import { badRequest, unauthorized, serverError, notFound, forbidden } from './components'
+import { accountSchema, loginParamsSchema, errorSchema, surveySchema, surveysSchema, surveyAnswerSchema, apiKeyAuthSchema } from './schemas'
 
 export default {
   openapi: '3.0.0',
@@ -18,19 +18,29 @@ export default {
   }],
   tags: [{
     name: 'Login'
+  }, {
+    name: 'Survey'
   }],
   paths: {
-    '/login': loginPath
+    '/login': loginPath,
+    '/surveys': surveyPath
   },
   schemas: {
     account: accountSchema,
     loginParams: loginParamsSchema,
-    error: errorSchema
+    error: errorSchema,
+    survey: surveySchema,
+    surveys: surveysSchema,
+    surveyAnswer: surveyAnswerSchema
   },
   components: {
+    securitySchemes: {
+      apiKeyAuth: apiKeyAuthSchema
+    },
     badRequest,
     unauthorized,
     serverError,
-    notFound
+    notFound,
+    forbidden
   }
 }
