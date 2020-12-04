b1b05c6258b6fc9672074396dc330ebdfcd72893
sSahli232 <sahli.selim@gmail.com>
2020-12-04 19:15:21 +0100

    refactor: move mockLoadSurveyResultRepository to a separate file

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -1,16 +1,6 @@
 import { DbLoadSurveyResult } from './db-load-survey-result'
-import { SurveyResultModel } from '../save-survey-result/db-save-survey-result-protocols'
-import { mockSurveyResultModel } from '@/domain/test/mock-survey-result'
 import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
-
-const mockLoadSurveyResultRepository = (): LoadSurveyResultRepository => {
-  class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
-    async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
-      return await Promise.resolve(mockSurveyResultModel())
-    }
-  }
-  return new LoadSurveyResultRepositoryStub()
-}
+import { mockLoadSurveyResultRepository } from '@/data/test/index'
 
 type SutTypes = {
   sut: DbLoadSurveyResult
