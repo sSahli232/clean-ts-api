df08da2d9c731b752a821527b4207ff674a75c29
sSahli232 <sahli.selim@gmail.com>
2021-01-13 11:54:34 +0100

    feat: add CheckSurveyById usecase

--- tests/infra/db/mongodb/survey-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/survey-mongo-repository.spec.ts
@@ -3,6 +3,7 @@ import { mockAddSurveyParams } from '@/tests/domain/mocks'
 import { mockAddAccountParams } from '@/tests/domain/mocks/mock-account'
 
 import { Collection } from 'mongodb'
+import FakeObjectId from 'bson-objectid'
 
 let surveyCollection: Collection
 let surveyResultCollection: Collection
@@ -81,6 +82,21 @@ describe('SurveyMongoRepository', () => {
       const survey = await sut.loadById(res.ops[0]._id)
       expect(survey).toBeTruthy()
       expect(survey.id).toBeTruthy()
+    })
+  })
+
+  describe('checkById()', () => {
+    test('Should return true if survey exists', async () => {
+      const res = await surveyCollection.insertOne(mockAddSurveyParams())
+      const sut = makeSut()
+      const exists = await sut.checkById(res.ops[0]._id)
+      expect(exists).toBe(true)
+    })
+
+    test('Should return false if survey not exists', async () => {
+      const sut = makeSut()
+      const exists = await sut.checkById(FakeObjectId.generate())
+      expect(exists).toBe(false)
     })
   })
 })
