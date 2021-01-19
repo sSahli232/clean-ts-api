d983b7edc23f7eb10239f417cae1f0419a15f877
sSahli232 <sahli.selim@gmail.com>
2021-01-19 13:31:16 +0100

    test: ensure SignUp Mutations returns an account on valid data

--- tests/main/graphql/login.test.ts
+++ tests/main/graphql/login.test.ts
@@ -62,5 +62,41 @@ describe('Login GraphQL', () => {
       expect(res.data).toBeFalsy()
       expect(res.errors[0].message).toBe('Unauthorized')
     })
+
+    test('Should return InvalidParamError on invalid email', async () => {
+      const { query } = createTestClient({ apolloServer })
+      const res: any = await query(loginQuery, {
+        variables: {
+          email: 'selim.sahli',
+          password: '123'
+        }
+      })
+      expect(res.data).toBeFalsy()
+      expect(res.errors[0].message).toBe('Invalid param: email')
+    })
+  })
+
+  describe('SignUp Mutation', () => {
+    const signUpMutation = gql`
+        mutation signUp ($name: String!, $email: String!, $password: String!, $passwordConfirmation: String!) {
+            signUp(name: $name, email: $email, password: $password, passwordConfirmation: $passwordConfirmation) {
+                accessToken
+                name
+            }
+        }
+      `
+    test('Should return an Account on valid data', async () => {
+      const { mutate } = createTestClient({ apolloServer })
+      const res: any = await mutate(signUpMutation, {
+        variables: {
+          name: 'Selim',
+          email: 'selim.sahli@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        }
+      })
+      expect(res.data.signUp.accessToken).toBeTruthy()
+      expect(res.data.signUp.name).toBe('Selim')
+    })
   })
 })
